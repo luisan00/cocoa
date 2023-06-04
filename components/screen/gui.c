35298a6a193396b4e1dd6dad70630c1a3aea39b5
luisan00 <luisan00@hotmail.com>
2023-06-04 13:27:58 +0200

    ...

--- components/screen/gui.c
+++ components/screen/gui.c
@@ -5,6 +5,7 @@
 #include "esp_timer.h"
 
 #include "lvgl.h"
+#include "lv_qrcode.h"
 
 #include "screen.h"
 #include "screen_config.h"
@@ -12,39 +13,11 @@
 #include "logger.h"
 
 void gui_init(lv_obj_t *scr) {
-    /*Create a container with ROW flex direction*/
-    lv_obj_t *cont_row = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(cont_row, 300, 75);
-    lv_obj_align(cont_row, LV_ALIGN_TOP_MID, 0, 5);
-    lv_obj_set_flex_flow(cont_row, LV_FLEX_FLOW_ROW);
+    lv_color_t bg_color = lv_color_white();
+    lv_color_t fg_color = lv_color_black();
+    lv_obj_t *qr = lv_qrcode_create();
+    lv_qrcode_update(qr, "hola", 5);
 
-    /*Create a container with COLUMN flex direction*/
-    lv_obj_t *cont_col = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(cont_col, 200, 150);
-    lv_obj_align_to(cont_col, cont_row, LV_ALIGN_OUT_BOTTOM_MID, 0, 5);
-    lv_obj_set_flex_flow(cont_col, LV_FLEX_FLOW_COLUMN);
-
-    uint32_t i;
-    for (i = 0; i < 5; i++) {
-        lv_obj_t *obj;
-        lv_obj_t *label;
-
-        /*Add items to the row*/
-        obj = lv_btn_create(cont_row);
-        lv_obj_set_size(obj, 100, LV_PCT(100));
-
-        label = lv_label_create(obj);
-        lv_label_set_text_fmt(label, "Item: %d", i);
-        lv_obj_center(label);
-
-        /*Add items to the column*/
-        obj = lv_btn_create(cont_col);
-        lv_obj_set_size(obj, LV_PCT(100), LV_SIZE_CONTENT);
-
-        label = lv_label_create(obj);
-        lv_label_set_text_fmt(label, "Item: %d", i);
-        lv_obj_center(label);
-    }
 }
 
 void gui_deinit(void) {}
