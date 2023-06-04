108c4c3b1991b80fe5c1b1dd922b191b3a201799
luisan00 <luisan00@hotmail.com>
2023-06-04 13:30:09 +0200

    ,.

--- components/screen/gui.c
+++ components/screen/gui.c
@@ -13,10 +13,9 @@
 #include "logger.h"
 
 void gui_init(lv_obj_t *scr) {
-    lv_color_t bg_color = lv_color_white();
-    lv_color_t fg_color = lv_color_black();
-    lv_obj_t *qr = lv_qrcode_create();
-    lv_qrcode_update(qr, "hola", 5);
+
+    lv_obj_t *qr = lv_qrcode_create(scr);
+
 
 }
 
