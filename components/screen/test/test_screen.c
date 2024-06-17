784086fe1f7a1aee993f1964fc4c8cd0bb2e916e
luisan00 <luisan00@hotmail.com>
2024-06-17 17:32:44 +0200

    more tests for the screen component

---
+++ components/screen/test/test_screen.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unity.h"
+
+#include "screen.h"
+#include "logger.h"
+
+#include "test_screen.h"
+
+static const char tag[] = "[screen]";
+
+// storage_stats
+TEST_CASE("mockup test case", tag) {
+
+    TEST_ASSERT_EQUAL_INT(0, 0);
+    TEST_ASSERT_EQUAL_INT(1, 1);
+    TEST_ASSERT_EQUAL_INT(2, 2);
+    TEST_ASSERT_EQUAL_INT(3, 3);
+}
--- test/main/main_test.c
+++ test/main/main_test.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include "unity.h"
 
-
 static void print_banner(const char *text);
 
 void app_main(void) {
@@ -16,6 +15,12 @@ void app_main(void) {
     UNITY_BEGIN();
     unity_run_tests_by_tag("[screen]", false);
     UNITY_END();
+
+    print_banner("Starting interactive test menu");
+    /* This function will not return, and will be busy waiting for UART input.
+     * Make sure that task watchdog is disabled if you use this function.
+     */
+    unity_run_menu();
 }
 
 static void print_banner(const char *text) {
