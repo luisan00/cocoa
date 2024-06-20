b7df45c473c09629bd9564c535b4b6140a32353a
luisan00 <luisan00@hotmail.com>
2024-06-20 23:44:22 +0200

    add doc skeleton

--- components/screen/test/test_screen.c
+++ components/screen/test/test_screen.c
@@ -1,5 +1,23 @@
-#include <string.h>
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_spiffs.h"
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_dma_utils.h"
+#include "lvgl.h"
+
+
+
+#include <string.h>
+
 #include <stdlib.h>
 
 #include "unity.h"
