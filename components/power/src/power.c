4f272b3653d14a8648d9c698b560e4f8e28e1436
luisan00 <luisan00@hotmail.com>
2025-04-16 21:32:51 +0200

    fixup

---
+++ components/power/src/power.c
@@ -0,0 +1,38 @@
+#include "esp_err.h"
+#include "power.h"
+
+int get_bit(uint8_t reg) {
+    //
+    return 0;
+    //
+}
+
+uint8_t get_byte(uint8_t reg) {
+    //
+    return 0;
+    //
+}
+
+uint8_t *get_buffer(uint8_t reg) {
+    //
+    return 0;
+    //
+}
+
+esp_err_t set_bit(uint8_t reg, uint8_t bit) {
+    //
+    return ESP_OK;
+    //
+}
+
+esp_err_t set_byte(uint8_t reg, uint8_t value) {
+    //
+    return ESP_OK;
+    //
+}
+
+esp_err_t set_buffer(uint8_t reg, uint8_t *buffer, int len) {
+    //
+    return ESP_OK;
+    //
+}
